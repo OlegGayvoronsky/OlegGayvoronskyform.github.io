7eaa30b0e4888c7f75667f405dbf8cc534480cb9
OlegGayvoronsky <113290639+OlegGayvoronsky@users.noreply.github.com>
2022-11-14 10:21:26 +0300

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -53,23 +53,3 @@ saveButton.addEventListener('click', function () {
     window.localStorage.setItem('timestamp', (new Date()).getTime());
    }, false);
    textarea.value = window.localStorage.getItem('value');
-
-$(function(){
-    $(".ajaxForm").submit(function(e){
-        e.preventDefault();
-        var href = $(this).attr("action");
-        $.ajax({
-            type: "POST",
-            dataType: "json",
-            url: href,
-            data: $(this).serialize(),
-            success: function(response){
-                if(response.status == "success"){
-                    alert("We received your submission, thank you!");
-                }else{
-                    alert("An error occured: " + response.message);
-                }
-            }
-        });
-    });
-});
